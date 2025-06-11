8239753bb7427c8ace21ee1e7c03c7e4812cec64
Pantha Acharjee <freelancerpantha@gmail.com>
2025-06-11 15:17:59 +0600

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+const server  = require("./app")
+const dotenv = require("dotenv")
+const connectDB = require("./config/Datebase")
+
+//DOTENV CONFIGURATION
+dotenv.config({path:"./config.env"})
+
+connectDB()
+
+server.listen(process.env.PORT, ()=>{
+    console.log(`Server is listen on ${process.env.PORT}`)
+})
