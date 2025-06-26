7d059333320f6f55f4028c199b931b1e70a927bb
Pantha Acharjee <freelancerpantha@gmail.com>
2025-06-26 17:18:02 +0600

    first commit

--- app.ts
+++ app.ts
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "50mb" }));
 const corsOptions = {
   origin: [
     'http://localhost:3000', // Your local development
-    'https://your-production-frontend.com' // Your production frontend
+    'https://erp-final-fontend.vercel.app' // Your production frontend
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
